8c26162de836f050ebdb47bec41206b2b8e1f9ed
evs-dev <evansmeath@gmail.com>
2021-04-24 16:44:48 +0100

    Add update frequency functionality

--- main.lua
+++ main.lua
@@ -1,7 +1,14 @@
-local fps
+local UPDATE_FREQUENCY = 0.5 -- seconds
+local fps = 0
+local timeSinceLastUpdate = 0
 
 function tick(dt)
- fps = 1 / dt
+ if timeSinceLastUpdate < UPDATE_FREQUENCY and fps > 0 then
+  timeSinceLastUpdate = timeSinceLastUpdate + dt
+ else
+  fps = 1 / dt
+  timeSinceLastUpdate = 0
+ end
 end
 
 function draw()
