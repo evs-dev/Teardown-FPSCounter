8caa0bf0f50e745e864d94f39ab23b896c14ad6f
evs-dev <evansmeath@gmail.com>
2021-04-24 16:53:43 +0100

    Add title and close button

--- options.lua
+++ options.lua
@@ -1,3 +1,16 @@
 function draw()
- 
+ UiTranslate(UiCenter(), 250)
+ UiAlign("center middle")
+
+ -- Title
+ UiFont("bold.ttf", 48)
+ UiText("FPSCounter Options")
+
+ -- Close button
+ UiTranslate(0, 100)
+ UiFont("regular.ttf", 26)
+ UiButtonImageBox("ui/common/box-outline-6.png", 6, 6)
+ if UiTextButton("Close", 200, 40) then
+  Menu()
+ end
 end
