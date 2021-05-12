27cfcfcbf07ceb3c2c593f9c30506de6c2b408a2
evs-dev <evansmeath@gmail.com>
2021-05-12 20:27:20 +0100

    Restore local variables and instead run init() manually in tick() (actually fix #2)
    Mod options shouldn't be different on a per-save basis, so they should be local.

--- main.lua
+++ main.lua
@@ -1,8 +1,21 @@
 #include "keys.lua"
 #include "util.lua"
 
+local PREFIX
+local HIGH_CONTRAST
+local ENABLE_SHOW_HIDE_KEYBIND
+local SHOW_HIDE_KEYBIND
+local ALIGNMENT
+local ALIGNMENT_IS_RIGHT
+local ALIGNMENT_IS_BOTTOM
+local DISTANCE_FROM_CORNER
+local UPDATE_FREQUENCY
+local NUM_DECIMAL_FIGURES
+local SIZE
+
 local fps = 0
 local timeSinceLastUpdate = 0
+local initted = false
 visible = true
 
 function init()
@@ -20,11 +33,17 @@ function init()
  NUM_DECIMAL_FIGURES = getNumDecimalFigures()
  SIZE = getSize()
 
+ initted = true
  -- Force FPS to be updated instantly rather than starting at 0
  timeSinceLastUpdate = UPDATE_FREQUENCY
 end
 
 function tick(dt)
+ -- Quick Load doesn't run init(), so do it here to avoid nil errors.
+ if not initted then
+  init()
+ end
+
  if timeSinceLastUpdate < UPDATE_FREQUENCY then
   timeSinceLastUpdate = timeSinceLastUpdate + dt
  elseif visible then
